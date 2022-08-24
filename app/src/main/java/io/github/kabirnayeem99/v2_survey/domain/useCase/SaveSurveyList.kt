add7bfb9ea9ebad8b48baafe8724b53d3df9752a
Naimul Kabir <39023212+kabirnayeem99@users.noreply.github.com>
2022-08-24 23:07:26 +0600

    📝 docs: add documentation

--- app/src/main/java/io/github/kabirnayeem99/v2_survey/domain/useCase/SaveSurveyList.kt
+++ app/src/main/java/io/github/kabirnayeem99/v2_survey/domain/useCase/SaveSurveyList.kt
@@ -6,9 +6,15 @@ import timber.log.Timber
 import java.util.*
 import javax.inject.Inject
 
-class SaveSurveyList @Inject constructor(
-    private val repository: SurveyRepository
-) {
+class SaveSurveyList @Inject constructor(private val repository: SurveyRepository) {
+
+    /**
+     * Saves all the answers of the survey in the local DB.
+     *
+     * @param id The time at which the survey was taken, also servers as a unique ID.
+     * @param answers that the users has answered in a single survey
+     * @return the id wrapped in a [Result] to indicate whether the save was successful or not
+     */
     suspend operator fun invoke(id: Long, answers: List<AnsweredSurvey>): Result<Long> {
         Timber.d("${Date(id)} -> Saving survey -> $answers")
         if (answers.isEmpty()) return Result.failure(Exception("No answers were provided."))
